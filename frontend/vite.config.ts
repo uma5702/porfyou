237a3ff7ab6a6e7e9d3a718174c50b63d60fae39
uma5702@gmail.com <uma5702@gmail.com>
2025-04-24 18:35:09 +0900

    deploy to gh-pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/porfyou/', // 예: '/porfyou-front/'
   plugins: [react()],
 })
